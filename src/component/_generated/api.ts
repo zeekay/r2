c9ff2283eb536ee14ad8b1b5aa3f7ccb2fe47a7f
Ian Macartney <ianhmacartney@gmail.com>
2025-11-05 22:52:54 -0800

    update codegen

--- src/component/_generated/api.ts
+++ src/component/_generated/api.ts
@@ -15,29 +15,39 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import { anyApi, componentsGeneric } from "convex/server";
+
+const fullApi: ApiFromModules<{
+  lib: typeof lib;
+}> = anyApi as any;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  lib: typeof lib;
-}>;
-
-export declare const api: FilterApi<
+export const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
->;
-export declare const internal: FilterApi<
+> = anyApi as any;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
->;
+> = anyApi as any;
 
-export declare const components: {
+export const components = componentsGeneric() as unknown as {
   actionRetrier: {
     public: {
       cancel: FunctionReference<
